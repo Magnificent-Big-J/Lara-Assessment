bfadcd2608a809bc91279fc97c9d441ce29a51d7
BigJ <joel.mnisi@protonmail.com>
2019-08-27 14:54:02 +0200

    Products and User seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,10 +11,10 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        /*$users = array(
+        $users = array(
             array('name'=>'Joel King','email'=>'joel@shopping.co.za','type'=>\App\User::USER_TYPE_CUSTOMER,'password'=>bcrypt('password')),
             array('name'=>'Kim Cloete','email'=>'kim@shopping.co.za','password'=>bcrypt('password')),
-        );*/
+        );
 
         $products = array(
             array('name'=>'Mad Giant Urban Legend','description'=>'Mad Giant Urban Legend - IPA - 24 x 340 ml','price'=>575,'discountPerc'=>0.5),
@@ -22,10 +22,10 @@ class UsersTableSeeder extends Seeder
             array('name'=>'Light Bulb','description'=>'Notebook:Light Bulbs','price'=>115,'discountPerc'=>0.25),
             array('name'=>'SanDisk USB','description'=>'SanDisk Cruzer Blade Flash Drive 16GB','price'=>4000,'discountPerc'=>0.5),
         );
-/*
+
         foreach ($users as $user){
             \App\User::create($user);
-        }*/
+        }
 
         foreach ($products as $product){
             \App\Product::create($product);
